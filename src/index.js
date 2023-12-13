25eccdf74b48b375819c0ffe1f51742c08d1b4d8
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2023-12-13 14:52:23 -0500

    Agregar funcionalidad a la caja de texto con boton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import analyzer from './analyzer.js';
+const btnClear=document.getElementById("reset-button");
+const text=document.querySelector("textarea[name='user-input']");
+
+btnClear.addEventListener("click",()=>{
+text.value = "";
+});
+
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
