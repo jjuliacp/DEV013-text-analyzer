2d41623ed8d5d4bdf627d5d2d8c5350aeb2abb7a
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2023-12-15 18:01:36 -0500

    Agregar funcion contador de palabras y caracteres

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,12 +1,37 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+   // console.log(text.split(' '));
+   let textArray = text.trim().split(' '); // arreglo que cuenta 
+   let count = 0; //contador
+  for (let i=0;i<textArray.length;i++){
+    if(textArray[i]!=""){
+      count++;
+    }
+  }
+   return count; //retorna resultado
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCount: (text) => {  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //console.log(text.length);
+    let textArray = text;
+    let count =0;
+    for (let i=0;i<textArray.length;i++){
+    if(textArray[i])count++;
+   }
+   return count;
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+  getCharacterCountExcludingSpaces: (text) => { //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //console.log(text.trim().replace(/\s+/g, '').length); NO CUENTA ESPACIOS SOLO CARACTERES
+    //console.log(text.trim().replace(/[^\w\s]/g, '').replace(/\s+/g, '').split('').length);
+    let textArray =text;
+    let count =0;
+    for (let i=0;i<textArray.trim().replace(/[^\w\s]/g, '').replace(/\s+/g, '').split('').length;i++){
+      if(textArray[i]){
+        count++;}
+    }
+    return count;
+
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import analyzer from './analyzer.js';
 const btnClear=document.getElementById("reset-button");
-const text=document.querySelector("textarea[name='user-input']");
+const boxText=document.querySelector("textarea[name='user-input']");
 
 btnClear.addEventListener("click",()=>{
-text.value = "";
+boxText.value = "";
 });
+
+// boxText.addEventListener("input", () =>{
+// //console.log(analyzer.getWordCount(boxText.value));
+// let wordCount=document.querySelector("li[data-testid='word-count'] span");
+// wordCount.textContent=analyzer.getWordCount(boxText.value);
+// });
+
+
+//palabras
+function wordEvent(){
+    let wordCount=document.querySelector("li[data-testid='word-count'] span");
+    wordCount.textContent=analyzer.getWordCount(boxText.value);
+     //analyzer.getWordCount("hola que tal");
+}
+boxText.addEventListener("input", wordEvent);
+
+// caracteres ---------
+function  characterEvent(){
+    let characterCount=document.querySelector("li[data-testid='character-count'] span");
+    characterCount.textContent=analyzer.getCharacterCount(boxText.value);
+    //analyzer.getCharacterCount("hola , . 0")
+}
+boxText.addEventListener("input", characterEvent);
+
+// Sin espacios ni signos---------
+function characterExcludingEvent(){
+    let characterExcluding=document.querySelector("li[data-testid='character-no-spaces-count'] span");
+    characterExcluding.textContent=analyzer.getCharacterCountExcludingSpaces(boxText.value);
+    //analyzer.getCharacterCountExcludingSpaces("h. a");
+}
+boxText.addEventListener("input",characterExcludingEvent);
+
+//contar  numeros
 
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
